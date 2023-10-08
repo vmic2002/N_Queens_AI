1de4e24bc8167ab066b931e283e63e8a6762d719
Victor Micha <victor.micha@mail.mcgill.ca>
2023-10-08 01:19:49 -0400

    Added colors to graph

--- graphData.py
+++ graphData.py
@@ -1,8 +1,10 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import pandas
+import pandas as pd
+import numpy as np
 
-points = pandas.read_csv('data.csv')
+# Load your data from 'data.csv'
+points = pd.read_csv('data.csv')
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -11,16 +13,20 @@ x = points['N'].values
 y = points['numThreads'].values
 z = points['time'].values
 
-#  x   y   z  
-# 1.1,1.2,1.3
-# 2.1,2.2,2.3
-# 3.1,3.2,3.3
-# 4.1,4.2,4.3
+# Create a unique color for each numThreads value
+unique_numThreads = np.unique(y)
+colors = plt.cm.viridis(np.linspace(0, 1, len(unique_numThreads)))
 
-ax.scatter(x, y, z, c='r', marker='o')
+for i, numThread in enumerate(unique_numThreads):
+    mask = y == numThread
+    ax.scatter(x[mask], y[mask], z[mask], c=[colors[i]], label=f'numThreads = {numThread}', marker='o')
 
 ax.set_xlabel('N')
 ax.set_ylabel('numThreads')
 ax.set_zlabel('time (sec)')
 
+# Add a legend to distinguish different numThreads values
+ax.legend()
+
 plt.show()
+
